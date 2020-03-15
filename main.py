288ae1505d280260adc096fc9aaf8148bb9d623d
buptpanchang@gmail.com <42539366+PATCHPC@users.noreply.github.com>
2020-03-15 14:21:22 +0800

    frame done

---
+++ main.py
@@ -0,0 +1,41 @@
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.keys import Keys
+import time,re,csv
+from bs4 import BeautifulSoup
+# 数据抓取相关的库
+
+import smtplib
+from email.mime.text import MIMEText
+# email 用于构建邮件内容
+from email.header import Header
+# 用于构建邮件头
+
+def extract_html():
+    exit()
+
+def extract_data():
+    exit()
+
+def compare_data():
+    exit()
+
+def update_data():
+    exit()
+
+def send_email():
+    exit()
+
+if __name__ == '__main__':
+    from_addr = '1132680329@qq.com'
+    password = 'jncqbojqjdcujcbc'
+    to_addr = '1132680329@qq.com'
+    smtp_server = 'smtp.qq.com'
+    # 邮件相关
+
+    url = "https://iclass.bupt.edu.cn/webapps/portal/execute/tabs/tabAction?tab_tab_group_id=_1_1"
+    userid = '2018211197'
+    passwd = '065527'
+
+    browser = 
+
+
